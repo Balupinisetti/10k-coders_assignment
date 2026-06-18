3284431d79778a494f1bcd87568e90156368592f
Balu Pinisetti <pinisettibalu9951@gmail.com>
2026-06-18 15:26:40 +0530

    Add files via upload

---
+++ ASG3list.py
@@ -0,0 +1,45 @@
+#LIST CREATE AND APPEND VALUES
+# list=[]
+# list.append([1,2,3,4,5,6,7,8,9,10])
+# print(list)
+
+
+#ADDING 0 AT 0 INDEX
+# list=[1,2,3,4,5,6,7,8,9,10]
+# list.insert(0,0)
+# print(list)
+
+
+
+#SORTING THE LIST
+# list=[1,2,3,4,5,6,7,8,9,10]
+# list.sort()
+# print(list)
+
+#REVERSING THE ORDER
+# list=[1,2,3,4,5,6,7,8,9,10]
+# list.reverse()
+# print(list)
+
+
+#DELETING THE LAST ELEMENT AND STORING IN THE NEW VALUE
+# list=[1,2,3,4,5,6,7,8,9,10]
+# last_num=list.pop()
+# list.pop()
+# print(last_num)
+
+
+#FINDING THE INDEX OF 5
+# list=[1,2,3,4,5,6,7,8,9,10]
+# print(list.index(5))
+
+
+#EMPTY THE LIST AND PRINTING THE SPACE
+# list=[1,2,3,4,5,6,7,8,9,10]
+# list.clear()
+# print(len(list))
+
+
+
+
+
