23d774d36d1e7dd9e219de2c5d8e7a4a7f019db0
Balu Pinisetti <pinisettibalu9951@gmail.com>
2026-06-23 14:05:51 +0530

    Add files via upload

---
+++ asgdict.py
@@ -0,0 +1,62 @@
+# dict adding key
+# Dictionarymanager={"balu":1,"bhaskar":2}
+# Dictionarymanager["bhask"]=3
+# print(Dictionarymanager)
+
+
+#dict del key
+# Dictionarymanager={"balu":1,"bhaskar":2}
+# Dictionarymanager.pop("balu")
+# print(Dictionarymanager)
+
+
+#updating dict
+# Dictionarymanager={"balu":1,"bhaskar":2}
+# Dictionarymanager["bhask"]=3
+# print(Dictionarymanager)
+# Dictionarymanager["balu"]=101
+# print(Dictionarymanager)
+
+
+#accessing with key through value
+# Dictionarymanager={"balu":1,"bhaskar":2}
+# res=Dictionarymanager["balu"]
+# print(res)
+# res1=Dictionarymanager["bhaskar"]
+# print(res1)
+
+
+
+
+# Dictionarymanager={"balu":1,"bhaskar":2}
+# Dictionarymanager.clear()
+# print(Dictionarymanager)
+# Dictionarymanager.setdefault("bhaskarbava",143)
+# print(Dictionarymanager)
+
+
+
+
+
+
+# class DictionaryManager:
+#     def __init__(self):
+#         self.dictionary = {"balu": 1, "bhaskar": 2}
+
+#     def get_key(self, value):
+#         for key, val in self.dictionary.items():
+#             if val == value:
+#                 return key
+#         return "Value not found"
+
+
+# def main():
+#     manager = DictionaryManager()
+
+#     print(manager.get_key(1))
+#     print(manager.get_key(2))
+
+
+# if __name__ == "__main__":
+#     main()
+
