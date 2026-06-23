38df11e5282f8fd9f57ae55d8535f5b25e13f969
Balu Pinisetti <pinisettibalu9951@gmail.com>
2026-06-23 14:18:23 +0530

    Add files via upload

---
+++ asgScope.py
@@ -0,0 +1,11 @@
+x=10      #x value
+y=5        #y value
+
+def cal_sum():   #creating function cal_sum
+    z=x+y        #assign value of x+y to z
+    return z      #returning value in z
+print(cal_sum())   #calling the function and printing
+
+
+#output
+15
