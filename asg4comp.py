682b7bc40874deb883f63e5813f64a7447896713
Balu Pinisetti <pinisettibalu9951@gmail.com>
2026-06-28 18:04:51 +0530

    Add files via upload

---
+++ asg4comp.py
@@ -0,0 +1,12 @@
+
+# res=[i**2 for i in range(1,11)]
+# print(res)
+
+# num=[1,2,3,4,5,6,6,5,3,2]
+# res=set([i**2 for i in range(len(num))])
+# print(res)
+
+
+# num={1,2,3,4,5,6}
+# res={i:i**2 for i in range(len(num))}
+# print(res)
